c3c69720af7db26827f19b382b8fd781f7ed3484
yashk <devyk100@gmail.com>
2024-05-17 09:30:26 +0530

    reduce() undefined error

--- app/networking/index.ts
+++ app/networking/index.ts
@@ -3,13 +3,14 @@ import axios, { AxiosResponse } from 'axios';
 import {Dirs, FileSystem} from 'react-native-file-access';
 import { getBottomIndexCount, getJwt, retrieveAllData, setBottomIndex } from '../localStorage';
 import { Alert, ToastAndroid } from 'react-native';
+import 'dotenv/config'
 // const response = await axios.post(
 //   'http://10.0.2.2:3000/api/v1/sync',
 //   {
 //     fileData: fileData
 //   }
 // );
-export const BACKEND_URL = "something/";
+export const BACKEND_URL = "localhost:8080/";
 const sendData = async (data: any) => {
   const jwt = getJwt();
   const response = await axios.post(
@@ -28,21 +29,33 @@ const sendData = async (data: any) => {
 }
 
 const sendImage = async (fileName: string, dataId: number) => {
-  const fileData = await FileSystem.readFile(fileName, 'base64');
-  const jwt = getJwt();
-  const response = await axios.post(
-    BACKEND_URL + 'api/v1/sync/image/',
-    {
-      fileData: fileData,
-      dataId: dataId
-    }
-    ,{
-      headers: {
-        Authorization: `Bearer ${jwt}`
+  try{
+
+    const fileData = await FileSystem.readFile(fileName, 'base64');
+    const jwt = getJwt();
+    const response = await axios.post(
+      BACKEND_URL + 'api/v1/sync/image/',
+      {
+        fileData: fileData,
+        dataId: dataId
       }
-    }
-  )
-  return response
+      ,{
+        headers: {
+          Authorization: `Bearer ${jwt}`
+        }
+      }
+    )
+    return response
+  }
+  catch(error){
+    console.log("an error occured")
+    Alert.alert("And error with "+  error +  " occured");
+    return {
+      data:{
+        success: false
+      }
+    } as AxiosResponse<any, any>
+  }
 }
 
 const sendAnEntry = async (data: any) => {
@@ -63,7 +76,6 @@ const sendAnEntry = async (data: any) => {
 }
 
 export const upload = async () => {
-  // console.log(fileName);
   const dataArray = retrieveAllData()
   let counter = 1;
   for(let a of dataArray){
